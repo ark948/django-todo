f05525a9b511d88b1896657e288a7dd0d8ee4fab
ark948 <armankarimi948@gmail.com>
2024-07-19 12:57:49 +0330

    updated templates and pages:index view

--- pages/urls.py
+++ pages/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-from pages.views import index
+from pages.views import HomePageView
 
 app_name = "pages"
 urlpatterns = [
-    path("", index, name="index"),
+    path("", HomePageView.as_view(), name="index"),
 ]
--- pages/views.py
+++ pages/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.generic import TemplateView
 
 # Create your views here.
 
-def index(request):
-    return HttpResponse("welcome")
+class HomePageView(TemplateView):
+    template_name = "pages/index.html"

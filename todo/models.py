54f5050589edb8bea3922e11ad8fc91982c1e55f
ark948 <armankarimi948@gmail.com>
2024-07-20 15:45:11 +0330

    Alter field status on task (todo app)

--- todo/models.py
+++ todo/models.py
@@ -19,5 +19,5 @@ class Task(models.Model):
     title = models.CharField("Title", null=False, blank=False, max_length=50)
     details = models.TextField("Details", null=True, blank=True)
     created = models.DateTimeField("Created on", auto_now_add=True)
-    status = models.OneToOneField(TaskStatus, on_delete=models.CASCADE)
+    status = models.ForeignKey(TaskStatus, unique=False, on_delete=models.CASCADE)
     owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="tasks")
--- todo/views.py
+++ todo/views.py
@@ -1,7 +1,13 @@
+from typing import Any
+from django.db.models.query import QuerySet
 from django.shortcuts import render
 from django.views.generic import TemplateView, ListView
+from todo.models import Task
 
 # Create your views here.
 
-class TodoHomePage(TemplateView):
+class TodoHomePage(ListView):
     template_name = "todo/index.html"
+
+    def get_queryset(self) -> QuerySet[Any]:
+        return Task.objects.filter(owner__pk=self.request.user.pk)
